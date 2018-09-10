8aa3d2d7f0162768bc9654b4aa098c21c47be97e
Gustavo Vieira <gustavovieiradeoliveira@gmail.com>
2018-09-10 18:49:14 -0300

    Add files via upload

---
+++ apresentacao.py
@@ -0,0 +1,6 @@
+"""
+Ola! meu nome é Gustavo Vieira de Oliveira, moro no Brasil, no estado do Rio de Janeiro, tenho conhecimento em:
+Python(Data Science e Machine Learning), html, CSS, sql e Php; essa é minha pequena apresentação;
+minha e dos meus projetos que segue dentro do meu portifoleo, para mais informação, meu e-mail é
+gustavovieiradeoliveira@gmail.com
+"""
