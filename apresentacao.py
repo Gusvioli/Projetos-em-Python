da3151ac4cb9c662404b526b4d8a09d345840eb9
Grallyan <gustavovieiradeoliveira@gmail.com>
2021-12-25 17:29:19 -0300

    Grallyan

--- apresentacao.py
+++ apresentacao.py
@@ -2,7 +2,7 @@
 Sou Gustavo Vieira, nasci no Rio de Janeiro - RJ vivo no bairro da Ilha do Governador, aos 16 anos descobrir um forte interesse 
 por computadores e programação, tenho curiosidade em saber como funciona as coisas, nessa idade comecei aprender HTML estudei 
 por um ano, depois me interessei por CSS e PHP desde então vivo aprendendo linguagens de programação e tudo o que puder sobre 
-computadores, luto pra alcançar meus objetivos; tenho conhecimento em Python, Html, CSS, Php; essa é minha pequena apresentação;
-minha e dos meus projetos que segue dentro do meu portifoleo, para mais informação, meu e-mail é
+computadores, luto pra alcançar meus objetivos; tenho conhecimento em Python, Html, CSS, Php; e hoje em dia estou cursando escola de programação Trybe;
+essa é minha pequena apresentação; minha e dos meus projetos que segue dentro do meu portifoleo, para mais informação, meu e-mail é
 gustavovieiradeoliveira@gmail.com
 """
