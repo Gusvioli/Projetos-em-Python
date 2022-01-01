e3f2d3b8b2da582b70b685020b1976dda62e6eb6
Gustavo Vieira <gustavovieiradeoliveira@gmail.com>
2022-01-01 13:14:18 -0300

    Grallyan

---
+++ Python/escolhaUmNumero.py
@@ -0,0 +1,11 @@
+import random as rd
+
+while True:
+    x = rd.randint(0, 10)
+    inx = int(input("Digite um numero:"))
+    if inx == x:
+        print("Seu chute acertou!!!")
+        break
+    else:
+        print("O numero é {} Não foi dessa vez tente novamente, digite outro numero:".format(x))
+        continue
