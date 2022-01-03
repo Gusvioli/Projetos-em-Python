02c11cfb461756beb500e234c3592c1945e64d9d
Gustavo Vieira <gustavovieiradeoliveira@gmail.com>
2022-01-02 23:31:46 -0300

    Grallyan

--- Python/Jogo_Loteria.py
+++ Python/Jogo_Loteria.py
@@ -21,76 +21,129 @@ while lo <= 10:
     n = []
     n.append(dma)
     if qual_Jogo == "0":
-        numero = input("Digite quantos numeros voce gostaria de jogar de 6 a 15:")
-        if int(numero) >= 5 and int(numero) <= 15:
+
+        numero = input(
+            "Digite quantos numeros voce gostaria de jogar de 6 a 15:")
+        if int(numero) >= 6 and int(numero) <= 15:
+            n.append("Seu jogo é Mega sena")
             while x <= int(valor):
                 n.append(random.sample(range(1, 60), int(numero)))
                 x += 1
-            print("\n{}\nJogos: \n{}\n".format(n[0], n[1:]))
+            print(
+                "\n---------\n{}\n{}\nJogos: \n{}\n---------".format(n[0], n[1], n[2:]))
         else:
+            print("-----------------------------------------")
             print("Digite de 6 a 15 numeros, tente novamente")
+            print("-----------------------------------------")
+
     elif qual_Jogo == "1":
-        numero = input("Digite quantos numeros voce gostaria de jogar de 5 a 15:")
+
+        numero = input(
+            "Digite quantos numeros voce gostaria de jogar de 5 a 15:")
         if int(numero) >= 5 and int(numero) <= 15:
+            n.append("Seu jogo é Quina")
             while x <= int(valor):
                 n.append(random.sample(range(1, 60), int(numero)))
                 x += 1
-            print("\n{}\nJogos: \n{}\n".format(n[0], n[1:]))
+            print(
+                "\n---------\n{}\n{}\nJogos: \n{}\n---------".format(n[0], n[1], n[2:]))
         else:
+            print("-----------------------------------------")
             print("Digite de 5 a 15 numeros, tente novamente")
+            print("-----------------------------------------")
 
     elif qual_Jogo == "2":
-        numero = input("Digite quantos numeros voce gostaria de jogar de 15 a 20:")
+
+        numero = input(
+            "Digite quantos numeros voce gostaria de jogar de 15 a 20:")
         if int(numero) >= 15 and int(numero) <= 20:
+            n.append("Seu jogo é Lotofácil")
             while x <= int(valor):
                 n.append(random.sample(range(1, 25), int(numero)))
                 x += 1
-            print("\n{}\nJogos: \n{}\n".format(n[0], n[1:]))
+            print(
+                "\n---------\n{}\n{}\nJogos: \n{}\n---------".format(n[0], n[1], n[2:]))
         else:
+            print("-----------------------------------------")
             print("Digite de 15 a 20 numeros, tente novamente")
+            print("-----------------------------------------")
+
     elif qual_Jogo == "3":
+
         numero = input("Digite quantos numeros voce gostaria de jogar max 50:")
         if int(numero) <= 50:
+            n.append("Seu jogo é Lotomania")
             while x <= int(valor):
                 n.append(random.sample(range(1, 100), int(numero)))
                 x += 1
-            print("\n{}\nJogos: \n{}\n".format(n[0], n[1:]))
+            print(
+                "\n---------\n{}\n{}\nJogos: \n{}\n---------".format(n[0], n[1], n[2:]))
         else:
+            print("-----------------------------------------")
             print("Digite 50 numeros, tente novamente")
+            print("-----------------------------------------")
+
     elif qual_Jogo == "4":
-        numero = input("Digite quantos numeros voce gostaria de jogar 10 numeros:")
+
+        numero = input(
+            "Digite quantos numeros voce gostaria de jogar 10 numeros:")
         if int(numero) == 10:
+            n.append("Seu jogo é Time mania")
             while x <= int(valor):
                 n.append(random.sample(range(1, 80), int(numero)))
                 x += 1
-            print("\n{}\nJogos: \n{}\n".format(n[0], n[1:]))
+            print(
+                "\n---------\n{}\n{}\nJogos: \n{}\n---------".format(n[0], n[1], n[2:]))
         else:
+            print("-----------------------------------------")
             print("Digite 10 numeros, tente novamente")
+            print("-----------------------------------------")
+
     elif qual_Jogo == "5":
-        numero = input("Digite quantos numeros voce gostaria de jogar de 6 a 15:")
+
+        numero = input(
+            "Digite quantos numeros voce gostaria de jogar de 6 a 15:")
         if int(numero) >= 6 and int(numero) <= 15:
+            n.append("Seu jogo é Dupla sena")
             while x <= int(valor):
                 n.append(random.sample(range(1, 50), int(numero)))
                 x += 1
-            print("\n{}\nJogos: \n{}\n".format(n[0], n[1:]))
+            print(
+                "\n---------\n{}\n{}\nJogos: \n{}\n---------".format(n[0], n[1], n[2:]))
         else:
+            print("-----------------------------------------")
             print("Digite de 6 a 15 numeros, tente novamente")
+            print("-----------------------------------------")
+
     elif qual_Jogo == "6":
-        numero = input("Digite quantos numeros voce gostaria de jogar de 7 a 15:")
+
+        numero = input(
+            "Digite quantos numeros voce gostaria de jogar de 7 a 15:")
         if int(numero) >= 7 and int(numero) <= 15:
+            n.append("Seu jogo é Dia de sorte")
             while x <= int(valor):
                 n.append(random.sample(range(1, 31), int(numero)))
                 x += 1
-            print("\n{}\nJogos: \n{}\n".format(n[0], n[1:]))
+            print(
+                "\n---------\n{}\n{}\nJogos: \n{}\n---------".format(n[0], n[1], n[2:]))
         else:
+            print("-----------------------------------------")
             print("Digite de 7 a 15 numeros, tente novamente")
+            print("-----------------------------------------")
+
     else:
-        numero = input("Digite quantos numeros voce gostaria de jogar de 7 a 21:")
+
+        numero = input(
+            "Digite quantos numeros voce gostaria de jogar de 7 a 21:")
         if int(numero) >= 7 and int(numero) <= 21:
+            n.append("Seu jogo é Super sete")
             while x <= int(valor):
                 n.append(random.sample(range(1, 9), int(numero)))
                 x += 1
-            print("\n{}\nJogos: \n{}\n".format(n[0], n[1:]))
+            print(
+                "\n---------\n{}\n{}\nJogos: \n{}\n---------".format(n[0], n[1], n[2:]))
         else:
+            print("-----------------------------------------")
             print("Digite de 7 a 21 numeros, tente novamente")
-    lo+=1
+            print("-----------------------------------------")
+    lo += 1
