81b1758f4554497caab7b9994bccbe0448b0e0c9
Gustavo Vieira <gustavovieiradeoliveira@gmail.com>
2022-02-08 17:50:39 -0300

    Adiciono um novo programa que verifica se a porta de rede do ip x está aberta ou fechada

---
+++ Python/Verificar_porta.py
@@ -0,0 +1,17 @@
+import sys
+import socket
+import os
+
+ipAddress = '187.67.172.51'
+
+for ports in range(1, 65535):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    if s.connect_ex((ipAddress, ports)) == 0:
+        print(f"Porta {ports} Aberta!")
+        arquivos = open("Porta_aberta.txt", "a")
+        arquivos.write(f"\nPorta {ports} Aberta!")
+        arquivos.close()
+        s.close()
+    else:
+        print(f"Porta {ports} Fechada!")
