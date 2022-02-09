b10c8cc6f71cacdbff6a964767bdc633513e5e96
Gustavo Vieira <gustavovieiradeoliveira@gmail.com>
2022-02-09 17:42:37 -0300

    Um pouco de interação

--- Python/Verificar_porta.py
+++ Python/Verificar_porta.py
@@ -1,19 +1,41 @@
 import socket
 import os
 
-ipAddress = '187.67.172.51'
+entrada = input("1=Disparar teste de portas ou 2=Teste de porta unitária:")
+ipAddress = input("Digite o IP:")
 
-for ports in range(1, 65535):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if entrada == "1":
+    for ports in range(1, 65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    if s.connect_ex((ipAddress, ports)) == 0:
-        print(f"Porta {ports} Aberta!")
-        arquivos = open("Portas_abertas.txt", "a")
-        arquivos.write(f"\nPorta {ports} Aberta!")
-        arquivos.close()
-        s.close()
-    else:
-        print(f"Porta {ports} Fechada!")
-        arquivos = open("Portas_Fechadas.txt", "a")
-        arquivos.write(f"\nPorta {ports} Fechada!")
-        arquivos.close()
+        if s.connect_ex((ipAddress, ports)) == 0:
+            print(f"Porta {ports} Aberta!")
+            arquivos = open("Portas_abertas.txt", "a")
+            arquivos.write(f"\nPorta {ports} Aberta!")
+            arquivos.close()
+            s.close()
+        else:
+            print(f"Porta {ports} Fechada!")
+            arquivos = open("Portas_Fechadas.txt", "a")
+            arquivos.write(f"\nPorta {ports} Fechada!")
+            arquivos.close()
+else:
+    pass
+if entrada == "2":
+    while True:
+        entrada_ip = input("Digite a PORTA:")
+        try:
+            if int(entrada_ip) >= 0 and int(entrada_ip) <= 65535:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                ver_s = s.connect_ex((ipAddress, int(entrada_ip)))
+                if ver_s == 0:
+                    print(f"Porta {entrada_ip} Aberta!")
+                else:
+                    print(f"Porta {entrada_ip} Fechada!")
+            else:
+                print("PROGRAMA ENCERRADO")
+                break
+        except:
+            pass
+else:
+    pass
