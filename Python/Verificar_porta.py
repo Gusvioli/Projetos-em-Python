6fe8f6cac49469701214c4ffd7d0ee1dc8bb020d
Gustavo Vieira <gustavovieiradeoliveira@gmail.com>
2022-02-09 06:43:03 -0300

    Atualização

--- Python/Verificar_porta.py
+++ Python/Verificar_porta.py
@@ -8,9 +8,12 @@ for ports in range(1, 65535):
 
     if s.connect_ex((ipAddress, ports)) == 0:
         print(f"Porta {ports} Aberta!")
-        arquivos = open("Porta_aberta.txt", "a")
+        arquivos = open("Portas_abertas.txt", "a")
         arquivos.write(f"\nPorta {ports} Aberta!")
         arquivos.close()
         s.close()
     else:
         print(f"Porta {ports} Fechada!")
+        arquivos = open("Portas_Fechadas.txt", "a")
+        arquivos.write(f"\nPorta {ports} Fechada!")
+        arquivos.close()
