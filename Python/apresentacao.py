48e767183ba634d4a8dda8459760d79a5ed7e1cf
Gustavo Vieira <gustavovieiradeoliveira@gmail.com>
2022-03-05 14:21:11 -0300

    Grallyan

--- Python/apresentacao.py
+++ Python/apresentacao.py
@@ -4,5 +4,5 @@ por computadores e programação, tenho curiosidade em saber como funcionam as c
 por um ano, depois me interessei por CSS e PHP desde então vivo aprendendo linguagens de programação e tudo o que puder sobre 
 computadores. Luto para alcançar meus objetivos; tenho conhecimento em Python, Html, CSS, Php e hoje em dia estou cursando escola de programação Trybe.
 Essa é minha pequena apresentação, minha e dos meus projetos que seguem dentro do meu portifoleo. Para mais informações meu e-mail é
-gustavovieiradeoliveira@gmail.com.
+gustavovieiradeoliveira@gmail.com
 """
