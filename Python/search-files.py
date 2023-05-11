920a573b2b91144438eb8b14ec2184906cde7dbf
Gustavo Vieira de Oliveira <gustavovieiradeoliveira@gmail.com>
2023-05-11 08:18:31 -0300

    Update search-files.py

--- Python/search-files.py
+++ Python/search-files.py
@@ -1,21 +1,37 @@
+# import os
+
+# def file_search(name, path):
+#     def files_path06(*args):
+#         l = []
+#         for item in args:
+#             for p, _, files in os.walk(os.path.abspath(item)):
+#                 l.append([p, files])
+#         return l
+
+#     def files_path():
+#         arr = []
+#         for verTxt in files_path06(path)[0][1]:
+#             loc = path + verTxt
+#             string1 = name
+#             file1 = open(loc, "r", errors = 'ignore')
+#             readfile = file1.read()
+#             if string1 in readfile:
+#                 arr.append(loc)
+#             file1.close() 
+#         return arr
+    
+#     return files_path()
+
+# ChatGPT: Código acima foi melhorado com GPT-3
 import os
 
-def files_path06(*args):
-    l = []
-    for item in args:
-        for p, _, files in os.walk(os.path.abspath(item)):
-            l.append([p, files])
-    return l
+def file_search(name, path):
+    arr = []
+    for filename in os.listdir(path):
+        fullpath = os.path.join(path, filename)
+        if os.path.isfile(fullpath) and name in open(fullpath, errors='ignore').read():
+            arr.append(fullpath)
+    return arr
 
 
-pegaArr = files_path06('./TXTs/')[0][1]
-arr = []
-for verTxt in pegaArr:
-    loc = './TXTs/' + verTxt
-    string1 = 'gustavovieiradeoliveira@gmail.com'
-    file1 = open(loc, "r", errors = 'ignore')
-    readfile = file1.read()
-    if string1 in readfile:
-        arr.append(loc)
-    file1.close() 
-print(arr)
+print(file_search('Mission', './'))
