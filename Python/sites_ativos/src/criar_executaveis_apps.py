cde0df61a6d8f691c5c3a0312fc872c723776ceb
Gusvioli <gustavovieiradeoliveira@gmail.com>
2023-12-27 16:59:31 -0300

    Updates

--- Python/sites_ativos/src/criar_executaveis_apps.py
+++ Python/sites_ativos/src/criar_executaveis_apps.py
@@ -1,4 +1,8 @@
+# Lista de nomes de arquivos
+nomes_arquivos = ['app_2.py', 'app_3.py', 'app_4.py', 'app_5.py', 'app_6.py', 'app_7.py', 'app_8.py', 'app_9.py', 'app_10.py', 'app_11.py', 'app_12.py', 'app_13.py', 'app_14.py', 'app_15.py', 'app_16.py', 'app_17.py', 'app_18.py', 'app_19.py', 'app_20.py', 'app_21.py', 'app_22.py', 'app_23.py', 'app_24.py', 'app_25.py', 'app_26.py']
 
+def codigo_python(number_of_characters, nome_url_http='nome_url_http', dominios='dominios'):
+    return  f"""
 import itertools
 import time
 import pandas as pd
@@ -6,7 +10,7 @@ import requests as rqs
 
 from cria_dominio import dominio
 
-number_of_characters = 3
+number_of_characters = {number_of_characters}
 
 data_dominios = './data/dominios_mais_usados.json'
 data_dominios_json = pd.read_json(data_dominios)
@@ -19,7 +23,7 @@ def gerar_combinacoes(characters, dominios, tamanho):
 
 def realizar_requisicao(nome_url_http, dominios):
     try:
-        url = f'http://{nome_url_http}{dominios}'
+        url = f'http://{{nome_url_http}}{{dominios}}'
         r_http = rqs.get(url, timeout=3)
         dominio(nome_url_http, r_http.status_code, r_http.headers, r_http.url, number_of_characters)
     except Exception as e:
@@ -33,4 +37,10 @@ def main():
         time.sleep(0.5)
 
 if __name__ == "__main__":
-    main()
+    main()"""
+
+x = 2
+for nome_arquivo in nomes_arquivos:
+    with open(f'src/{nome_arquivo}', 'w') as arquivo:
+        arquivo.write(codigo_python(x))
+    x += 1
--- Python/sites_ativos/src/limpar_duplicatas.py
+++ Python/sites_ativos/src/limpar_duplicatas.py
@@ -1,5 +1,5 @@
 import json
-import pandas as pd
+import os
 import time
 
 dataCriacao = time.strftime("%d/%m/%Y %H:%M:%S")
@@ -8,18 +8,43 @@ class ProductEncoder(json.JSONEncoder):
     def default(self, obj):
         return obj.__dict__
 
-def limpar_duplicatas(p1):
-    new_dict = pd.read_json(f'data/dominios/dominios{p1}.json') 
-    seu_dict = new_dict['dominios'].to_list()
-    unique_list = list()
-    for i in range(len(seu_dict)):
-        if seu_dict[i] not in seu_dict[i + 1:]:
-            unique_list.append(seu_dict[i])
+def listar_arquivos_em_diretorio(caminho_diretorio):
+    try:
+        lista = []
+        arquivos = os.listdir(caminho_diretorio)
+        
+        for arquivo in arquivos:
+            lista.append(arquivo)
+            
+        return lista
+    except FileNotFoundError:
+        print(f"O diretório '{caminho_diretorio}' não foi encontrado.")
+        return None
 
-    with open(f'data/dominios/dominios{p1}.json', 'w') as json_file:
+def remover_duplicatas(arquivo_json):
+    with open(arquivo_json, 'r') as json_file:
+        data = json.load(json_file)
+
+    unique_names = set()
+    unique_dominios = []
+
+    for dominio in data["dominios"]:
+        name = dominio["name_url_http"]
+        if name not in unique_names:
+            unique_names.add(name)
+            unique_dominios.append(dominio)
+
+    with open(arquivo_json, 'w') as json_file:
         json.dump(
-            {"data_criacao": dataCriacao, "dominios": unique_list},
+            {"data_criacao": dataCriacao, "dominios": unique_dominios},
             json_file,
             ensure_ascii=False,
-            indent=2, cls=ProductEncoder
+            indent=2,
+            cls=ProductEncoder
         )
+
+def remover_de_todos(all_json):
+    for x in all_json:
+        remover_duplicatas(f'data/dominios/{x}')
+
+remover_de_todos(listar_arquivos_em_diretorio('data/dominios'))
